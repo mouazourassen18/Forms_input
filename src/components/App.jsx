f8d4b7b685b9cb36e772a2062b99f8359a8df8f6
mouazourassen18 <54610297+mouazourassen18@users.noreply.github.com>
2021-09-02 21:21:35 -0400

    final review

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [inText, setText] = useState("");
+  const [newText, setN] = useState("");
+  function update(event) {
+    setText(event.target.value);
+  }
+  function newUp(event) {
+    setText(event.target.value);
+    setN(inText);
+  }
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>hello : {newText}</h1>
+      <input onChange={update} type="text" placeholder="What's your name?" />
+      <button onMouseOver={newUp}>Submit</button>
     </div>
   );
 }
